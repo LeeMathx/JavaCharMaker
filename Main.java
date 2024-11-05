19cb9a68735b38928bf1b344e1e972845fde6d0b
Noah Parker <guiltyredeemedpaul@gmail.com>
2024-11-05 11:55:39 -0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.Date;
+
+public class Main() {
+  public static void main(String[] args) {
+
+    String dateOfCreation = new Date();
+
+    System.out.println(
+    
+    public class charNames {
+      
+    }
+    
+    public class charInfo {
+
+      /*Array, "list" of character info
+      Name, Age, Date of Creation*/
+      String[] characters = {{Noah, 13, dateOfCreation}};
+        
+    }
+    
+  }
+}
